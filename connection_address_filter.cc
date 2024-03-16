2866dd52081d638e2fc4b4a1e1d397b4cbd80f06
Dino Fizzotti <dino@dinofizzotti.com>
2024-03-16 18:08:26 +0000

    Playing around with port check and connection close

--- connection_address_filter.cc
+++ connection_address_filter.cc
@@ -23,24 +23,29 @@ Network::FilterStatus ConnectionAddresssFilter::onNewConnection() {
   ENVOY_CONN_LOG(trace, "New connection established. Local: {}, Remote: {}",
                  read_callbacks_->connection(), local_address, remote_address);
   
-  const auto& remote = connection_info->remoteAddress();
+  // const auto& remote = connection_info->remoteAddress();
 
   Network::Address::CidrRange rng = Network::Address::CidrRange::create("127.0.0.1/32"); 
   
-  if (rng.isInRange(*remote)) {
-    ENVOY_LOG(trace, "is in range");
-    read_callbacks_->connection().close(Network::ConnectionCloseType::Abort);
-    return Network::FilterStatus::StopIteration;
-  }
+  // if (rng.isInRange(*remote)) {
+  //   ENVOY_LOG(trace, "is in range");
+  //   read_callbacks_->connection().close(Network::ConnectionCloseType::Abort);
+  //   return Network::FilterStatus::StopIteration;
+  // }
 
   // [dinofizz] WIP testing port filter
   // Check if the remote port is 8888
-  // if (connection_info->remoteAddress()->ip()->port() == 8888) {
-  //   ENVOY_LOG(trace, "Closing connection because remote IP is 127.0.0.1 and remote port is 8888");
-  //   read_callbacks_->connection().close(Network::ConnectionCloseType::Abort);
-  // } else {
-  //   ENVOY_LOG(trace, "NOT closing connection");
-  // }
+  if (connection_info->remoteAddress()->ip()->port() != 8888) {
+    ENVOY_LOG(trace, "Closing connection because remote IP is 127.0.0.1 and remote port is not 8888");
+    // read_callbacks_->connection().close(Network::ConnectionCloseType::Abort);
+    // return Network::FilterStatus::StopIteration;
+
+    read_callbacks_->connection().streamInfo().setConnectionTerminationDetails("access denied");
+    read_callbacks_->connection().close(Network::ConnectionCloseType::AbortReset, "forbidden_port_close");
+    return Network::FilterStatus::StopIteration;
+  } else {
+    ENVOY_LOG(trace, "NOT closing connection");
+  }
 
   return Network::FilterStatus::Continue;
 }
